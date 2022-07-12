03ea16ea614e29b81a576d067bdcd79f916d84b7
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 08:49:24 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio9
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                if ((x % 2) == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
